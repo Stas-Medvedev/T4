19bd3ca7e0d63da41078da9e6f91585484bb9ea8
Stas Medvedev <stan.medvedev@gmail.com>
2022-09-12 20:25:22 -0400

    Added player creation logic

--- Python/t4.py
+++ Python/t4.py
@@ -25,7 +25,7 @@ import Player
 def select_players():
         '''
         "Player vs player" or "player vs CPU" game mode selector.
-        Returns choice as a string which is passed to the start_game function.
+        Returns a pair of Player objects that will be used to create a Game object.
         '''
         choices = {1: ("player", "player"), 2: ("player", "cpu"), 3: ("cpu", "player")}
         print('1 - Player (X) vs Player (O)')
@@ -35,20 +35,30 @@ def select_players():
         while choice not in ['', '1', '2', '3']:
             choice = input('Select game mode ([1],2,3):')
         if choice == '':
-            return choices[1]
-        return choices[int(choice)]
+            selection = choices[1]
+        else:
+            selection = choices[int(choice)]
+        
+        players = []
+        for i in range(2):
+            if i == 0:
+                marker = 'X'
+            else:
+                marker = 'O'
+            if selection[i] == 'player':
+                player_name = input(f'Please enter name for player {i+1}:')
+                players.append(Player.Player(player_name, marker))
+            else:
+                player_name = 'CPU'
+                players.append(Player.CPU_Player(player_name, marker))
+            
+        return players
 
-def start_game(choices):
+def get_game_object(players):
     '''
     Accepts an interable with players, and returns a game object with those player types.
     '''
-    '''
-    This function is relying heavily on indexing which makes the code unreadable.
-    Perhaps define another function, get_player_object(), that returns a Player or CPU_Player object based on a
-    passed string.
-    '''
-    players = {'player':Player.Player(), 'cpu':Player.CPU_Player()}
-    return Game.Game(players[choices[0]], players[choices[1]])
+    return Game.Game(players[0], players[1])
 
 
 def check_restart():
