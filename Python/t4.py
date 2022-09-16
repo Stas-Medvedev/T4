f7c2b6fd2bc7a760295ad0ac0dcda380d99523a2
Stas Medvedev <stan.medvedev@gmail.com>
2022-09-15 21:17:47 -0400

    Added more player selection logic

--- Python/t4.py
+++ Python/t4.py
@@ -23,37 +23,80 @@
 import Game
 import Player
 
-def select_players():
-        '''
-        "Player vs player" or "player vs CPU" game mode selector.
-        Returns a pair of Player objects that will be used to create a Game object.
-        '''
-        choices = {1: ("player", "player"), 2: ("player", "cpu"), 3: ("cpu", "player")}
-        print('1 - Player (X) vs Player (O)')
-        print('2 - Player (X) vs CPU (O)')
-        print('3 - CPU (X) vs Player(O)')
+def get_human_player_object(marker):
+    '''
+    Returns a player object for human players.
+    Takes marker as an argument passed from the calling function to make sure
+    that the first player get 'X' and second gets 'O'.
+    This can be changed in the future, possibly allowing both players to play
+    with the same marker.
+    '''
+    name = input('Enter player name:')
+
+    return Player.Player(name, marker)
+
+def select_CPU_difficulty():
+    '''
+    Lets the user select CPU difficulty and returns a string that is used by
+    get_cpu_player_object() to return the appropriate CPU_Player object.
+    '''
+    difficulties = {'1':'easy', '2':'medium', '3':'hard'}
+    choice = input('Select CPU difficulty: [1] - Easy, 2 - Medium, 3 - Hard:')
+    while choice not in ['', '1', '2', '3']:
+            choice = input('Select CPU difficulty: [1] - Easy, 2 - Medium, 3 - Hard:')
+    if choice == '':
+        choice = '1'
+    
+    return difficulties[choice]
+
+def get_cpu_player_object(marker):
+    '''
+    Return a CPU_Player object based on selected difficulty.
+    '''
+    difficulty = select_CPU_difficulty()
+    if difficulty == 'easy':
+        return Player.Easy_CPU_Player('CPU', marker)
+    elif difficulty == 'medium':
+        return Player.Medium_CPU_Player('CPU', marker)
+    else:
+        return Player.Hard_CPU_Player('CPU', marker)
+
+def get_player_object(kind, marker):
+    if kind == 'human':
+        player = get_human_player_object(marker)
+    else:
+        player = get_cpu_player_object(marker)
+
+    return player
+
+def get_player_selection():
+    '''
+    Prompts the user to make a player type selection, and returns a tuple of player types
+    that is used to generate the appropriate player objects.
+    '''
+    player_pairs = {1: ("player", "player"), 2: ("player", "cpu"), 3: ("cpu", "player")}
+    print('1 - Player (X) vs Player (O)')
+    print('2 - Player (X) vs CPU (O)')
+    print('3 - CPU (X) vs Player(O)')
+    choice = input('Select game mode ([1],2,3):')
+    while choice not in ['', '1', '2', '3']:
         choice = input('Select game mode ([1],2,3):')
-        while choice not in ['', '1', '2', '3']:
-            choice = input('Select game mode ([1],2,3):')
-        if choice == '':
-            selection = choices[1]
-        else:
-            selection = choices[int(choice)]
+    if choice == '':
+        choice = '1'
+
+    return player_pairs[choice]
+    
+def get_players():
+    '''
+    "Player vs player" or "player vs CPU" game mode selector.
+    Returns a pair of Player objects that will be used to create a Game object.
+    '''
+    selection = get_player_selection()
+    
+    player1 = get_player_object(selection[0], 'X')
+    player2 = get_player_object(selection[1], 'O')
         
-        players = []
-        for i in range(2):
-            if i == 0:
-                marker = 'X'
-            else:
-                marker = 'O'
-            if selection[i] == 'player':
-                player_name = input(f'Please enter name for player {i+1}:')
-                players.append(Player.Player(player_name, marker))
-            else:
-                player_name = 'CPU'
-                players.append(Player.CPU_Player(player_name, marker))
-            
-        return players
+    return [player1, player2]
 
 def get_game_object(players):
     '''
