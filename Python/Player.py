d6d934b2012f273499c4656249e99743f7330c2b
Stas Medvedev <stan.medvedev@gmail.com>
2022-09-20 23:59:12 -0400

    Modified Medium CPU take_turn

--- Python/Player.py
+++ Python/Player.py
@@ -19,23 +19,6 @@ class Easy_CPU_Player(Player):
     def take_turn(self, available_positions):
         return random.choice(available_positions)
 
-class Medium_CPU_Player(Player):
-    '''
-    Medium CPU player randomly chooses between Easy and Hard CPU players' moves
-    '''
-    def take_turn(self, available_positions):
-        move_choice = random.choice([0,1])
-        if move_choice == 0:
-            self.easy_turn(available_positions)
-        else:
-            self.hard_turn(available_positions)
-
-    def easy_turn(self, available_positions):
-        return random.choice(available_positions)
-
-    def hard_turn(self, available_positions):
-        pass
-
 class Hard_CPU_Player(Player):
     '''
     Hard CPU player follows an algorithm to make sure to not lose a game
@@ -55,3 +38,14 @@ class Hard_CPU_Player(Player):
         Pick a random space. (will probably only be used one the first turn if center is unavailable)
         '''
         pass
+
+class Medium_CPU_Player(Hard_CPU_Player):
+    '''
+    Medium CPU player randomly chooses between Easy and Hard CPU players' moves
+    '''
+    def take_turn(self, available_positions):
+        move_choice = random.choice([0,1])
+        if move_choice == 0:
+            return random.choice(available_positions)
+        else:
+            super().take_turn()
