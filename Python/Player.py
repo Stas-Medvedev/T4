c86babdad9f4b87a9321bcba62e61c41824b43a9
Stas Medvedev <stan.medvedev@gmail.com>
2022-09-22 23:57:53 -0400

    Added WINNING_POSITIONS variable to Player classes

--- Python/Player.py
+++ Python/Player.py
@@ -4,6 +4,8 @@ class Player:
     '''
     Player class for human player.
     '''
+    WINNING_POSITIONS = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]]
+
     def __init__(self, name, marker):
         self.name = name
         self.marker = marker
