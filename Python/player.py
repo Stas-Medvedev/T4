ead8e65089bf5e79a2eedc8559a5aa676d3d6d35
Stas Medvedev <stan.medvedev@gmail.com>
2022-10-06 23:30:01 -0400

    Incorporated a Board object into Player classes

--- Python/player.py
+++ Python/player.py
@@ -5,20 +5,21 @@ class Player:
     Player class for human player.
     '''
     
-    def __init__(self, name, marker):
+    def __init__(self, name, marker, board):
         self.name = name
         self.marker = marker
+        self.board = board
 
-    def take_turn(self, available_positions):
-        position = int(input(f"{self.name}'s turn. Select a position {available_positions}:"))
+    def take_turn(self):
+        position = int(input(f"{self.name}'s turn. Select a position {self.board.available_positions}:"))
         return position
 
 class Easy_CPU_Player(Player):
     '''
     Easy CPU player selects an available space at random
     '''
-    def take_turn(self, available_positions):
-        return random.choice(available_positions)
+    def take_turn(self):
+        return random.choice(self.board.available_positions)
 
 class Hard_CPU_Player(Player):
     '''
@@ -32,8 +33,8 @@ class Hard_CPU_Player(Player):
         for positions in self.WINNING_POSITIONS:
             total = 0
             for position in positions:
-                if current_positions[position] == self.marker: total += 1
-                if current_positions[position] == ' ': blank = position
+                if self.board.current_positions[position] == self.marker: total += 1
+                if self.board.current_positions[position] == ' ': blank = position
             if total == 2: return blank + 1
         return 0
 
@@ -41,17 +42,17 @@ class Hard_CPU_Player(Player):
         for positions in self.WINNING_POSITIONS:
             total = 0
             for position in positions:
-                current_position = current_positions[position]
+                current_position = self.board.current_positions[position]
                 if current_position != self.marker and current_position != ' ':
                     total += 1
                 if current_position == ' ': blank = position
             if total == 2: return blank + 1
         return 0
 
-    def can_fork(available_positions):
+    def can_fork(self):
         pass
     
-    def take_turn(self, available_positions):
+    def take_turn(self):
         '''
         Can win: a winning position with 2 own markers and an empty space
         Need to cover: a winning position with 2 of opponent's markers and an empty space
@@ -71,9 +72,9 @@ class Medium_CPU_Player(Hard_CPU_Player):
     '''
     Medium CPU player randomly chooses between Easy and Hard CPU players' moves
     '''
-    def take_turn(self, available_positions):
+    def take_turn(self):
         move_choice = random.choice([0,1])
         if move_choice == 0:
-            return random.choice(available_positions)
+            return random.choice(self.board.available_positions)
         else:
             super().take_turn()
