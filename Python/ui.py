be2c7c8d6118ee3557439a19681d660deef094f4
Stas Medvedev <stan.medvedev@gmail.com>
2022-12-12 23:27:54 -0500

    Added more type hints and did a bit of clean-up

--- Python/ui.py
+++ Python/ui.py
@@ -1,5 +1,5 @@
-import player
-import game
+from player import Player, Easy_CPU_Player, Medium_CPU_Player, Hard_CPU_Player
+from game import Game
 from board import Board
 
 class UI:
@@ -20,19 +20,19 @@ class UI:
         
         return difficulties[choice]
 
-    def get_cpu_player_object(self, marker: str):
+    def get_cpu_player_object(self, marker: str) -> Player:
         '''
         Return a CPU_Player object based on selected difficulty.
         '''
         difficulty = self.select_CPU_difficulty()
         if difficulty == 'easy':
-            return player.Easy_CPU_Player('CPU', marker)
+            return Easy_CPU_Player('CPU', marker)
         elif difficulty == 'medium':
-            return player.Medium_CPU_Player('CPU', marker)
+            return Medium_CPU_Player('CPU', marker)
         else:
-            return player.Hard_CPU_Player('CPU', marker)
+            return Hard_CPU_Player('CPU', marker)
 
-    def get_human_player_object(marker):
+    def get_human_player_object(marker) -> Player:
         '''
         Returns a player object for human players.
         Takes marker as an argument passed from the calling function to make sure
@@ -42,9 +42,9 @@ class UI:
         '''
         name = input('Enter player name:')
 
-        return player.Player(name, marker)
+        return Player(name, marker)
 
-    def get_player_object(self, player_type, marker):
+    def get_player_object(self, player_type, marker) -> Player:
         if player_type == 'human':
             player = self.get_human_player_object(marker)
         else:
@@ -52,7 +52,7 @@ class UI:
 
         return player
 
-    def get_player_selection():
+    def get_player_selection() -> tuple:
         '''
         Prompts the user to make a player type selection, and returns a tuple of player types
         that is used to generate the appropriate player objects.
@@ -69,7 +69,7 @@ class UI:
 
         return player_pairs[choice]
         
-    def get_players(self):
+    def get_players(self) -> list:
         '''
         "Player vs player" or "player vs CPU" game mode selector.
         Returns a pair of Player objects that will be used to create a Game object.
@@ -81,11 +81,11 @@ class UI:
             
         return [player1, player2]
 
-    def get_game_object(self, players):
+    def get_game_object(self, players) -> Game:
         '''
         Accepts an interable with players, and returns a game object with those player types.
         '''
-        return game.Game(players[0], players[1], self.board)
+        return Game(players[0], players[1], self.board)
 
     def display_board(self, markers):
         '''
