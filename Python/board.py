e9eff721dc80ac705ca4efdaf35fcc9974facd69
Stas Medvedev <stan.medvedev@gmail.com>
2022-12-17 00:28:49 -0500

    Added update method to the Board class

--- Python/board.py
+++ Python/board.py
@@ -1,3 +1,10 @@
 class Board:
     available_positions = list(range(1,10))
     current_markers = [' '] * 9
+
+    def update(self, position: int, marker: str) -> None:
+        '''
+        Updates the board with the most recent marker.
+        '''
+        self.current_markers[position - 1] = marker
+        self.available_positions.remove(position)
