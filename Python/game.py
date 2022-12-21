7b65b3f24f7dee0c533fb85cb4274bd9615d9f0f
Stas Medvedev <stan.medvedev@gmail.com>
2022-12-20 23:30:38 -0500

    Modified the initializer for the Game class to create a new instance of Board instead of getting one as an argument

--- Python/game.py
+++ Python/game.py
@@ -1,12 +1,14 @@
+from board import Board
+
 class Game:
     winning_positions = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]]
 
-    def __init__(self, player1, player2, board):
+    def __init__(self, player1, player2):
         # pass two player objects upon initialization
         self.player1 = player1
         self.player2 = player2
         self.current_player = self.player1
-        self.board = board
+        self.board = Board()
 
     def change_current_player(self):
         self.current_player = self.player2 if self.current_player == self.player1 else self.player1
