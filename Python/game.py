00376dd16baf6b22caab953f6240404342794a34
Stas Medvedev <stan.medvedev@gmail.com>
2022-12-22 23:59:23 -0500

    Added to-do items

--- Python/game.py
+++ Python/game.py
@@ -1,5 +1,9 @@
 from board import Board
 
+# add protocols for Player and UI classes
+# add UI object implementation
+# add type hints
+
 class Game:
     winning_positions = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]]
 
