8cf0dc67059e8024639399225194053f688873f9
Stas Medvedev <stan.medvedev@gmail.com>
2023-01-13 23:48:49 -0500

    Add Board protocol

--- Python/interfaces.py
+++ Python/interfaces.py
@@ -1,3 +1,8 @@
 '''
 This file will contain all of the interfaces used in the program.
 '''
+from typing import Protocol, List
+
+class Board(Protocol):
+    available_positions: List[int]
+    current_markers: List[str]
