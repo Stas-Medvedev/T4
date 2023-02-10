73aed3764649c735c9fc596c8c5a835d1e7db9d0
Stas Medvedev <stan.medvedev@gmail.com>
2023-02-09 22:53:08 -0500

    Remove methods moved to GameManager

--- Python/ui.py
+++ Python/ui.py
@@ -46,54 +46,6 @@ class UI:
         
         return difficulties[choice]
 
-    # Move to GameManager
-    def get_cpu_player_object(self, marker: str) -> Player:
-        '''
-        Return a CPU_Player object based on selected difficulty.
-        '''
-        difficulty = self.select_CPU_difficulty()
-        if difficulty == 'easy':
-            return Easy_CPU_Player('CPU', marker)
-        elif difficulty == 'medium':
-            return Medium_CPU_Player('CPU', marker)
-        else:
-            return Hard_CPU_Player('CPU', marker)
-
-    # Move to GameManager and replace with get_plaayer_name()
-    def get_human_player_object(marker: str) -> Player:
-        '''
-        Returns a player object for human players.
-        Takes marker as an argument passed from the calling function to make sure
-        that the first player gets 'X' and second gets 'O'.
-        This can be changed in the future, possibly allowing both players to play
-        with the same marker.
-        '''
-        name = input('Enter player name:')
-
-        return Player(name, marker)
-
-    # Move to GameManager
-    def get_player_object(self, player_type: str, marker: str) -> Player:
-        if player_type == 'human':
-            player = self.get_human_player_object(marker)
-        else:
-            player = self.get_cpu_player_object(marker)
-
-        return player
-
-    # Move to GameManager
-    def get_players(self) -> list[Player]:
-        '''
-        "Player vs player" or "player vs CPU" game mode selector.
-        Returns a pair of Player objects that will be used to create a Game object.
-        '''
-        selection = self.get_player_selection()
-        
-        player1 = self.get_player_object(selection[0], 'X')
-        player2 = self.get_player_object(selection[1], 'O')
-            
-        return [player1, player2]
-
     def display_board(self, markers: list[str]) -> None:
         '''
         Displays the playing board with the provided markers.
