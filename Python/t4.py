c72f280bf2ec5362534b3c3f0d13a437aaa8e221
Stas Medvedev <stan.medvedev@gmail.com>
2023-02-18 23:56:39 -0500

    Update to-do list

--- Python/t4.py
+++ Python/t4.py
@@ -7,10 +7,6 @@
 #           - Game should probably create an instance of the UI class rather than the other way around
 #           - that means modifying the current setup of the UI initializer
 #           - that also means that Game class won't get player in the __init__ but at some point later
-#       Add type hints to functions
-#       Remove board as an object attribute in the player class and instead pass the current board 
-#           from the game class when calling the take_turn method
-#       Move all of the interfaces into an interface file
 #       Create GameManager class that will keep track of scores and create the necessary class instances
 #       UI class has display_board and display_current_board methods. Is the second one needed?
 #       Write unit tests
