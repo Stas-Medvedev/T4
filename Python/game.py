980bb0dadb047dff9f00f7d708fbeb95d4e99d0a
Stas Medvedev <stan.medvedev@gmail.com>
2023-03-14 23:28:37 -0400

    Update to-do list

--- Python/game.py
+++ Python/game.py
@@ -1,8 +1,6 @@
 from interfaces import Board, Player, UI
 
-# go through the current methods and add/remove functionality as necessary
-#   update methods to include changed Player.take_turn() functionality
-#   take into account functionality that will be handled by GameManager
+#  Update display_winner to use UI and Player class items
 
 class Game:
     winning_positions = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]]
