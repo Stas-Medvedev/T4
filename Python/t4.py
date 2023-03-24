722c93e0aad033e60955622f24bff0a81ade8169
Stas Medvedev <stan.medvedev@gmail.com>
2023-03-23 23:33:47 -0400

    Update to-do list

--- Python/t4.py
+++ Python/t4.py
@@ -1,16 +1,4 @@
 # TODO:
-#       work on separating and encorporating the responsibilities of the UI and Game classes
-#           - which one exactly does what?
-#           - if UI is responsible for displaying and intake all of the information,
-#             how do I integrate the Game class methods into it?
-#           - do I call the UI methods from within the Game class to run the game?
-#           - Game should probably create an instance of the UI class rather than the other way around
-#           - that means modifying the current setup of the UI initializer
-#           - that also means that Game class won't get player in the __init__ but at some point later
-#       Create GameManager class that will keep track of scores and create the necessary class instances
-#           - Create player and game instances inside GameManager
-#           - Remove player creation/selection functionality from the Game class
-#       UI class has display_board and display_current_board methods. Is the second one needed?
 #       Write unit tests
 #       Start running the code to debug
 
