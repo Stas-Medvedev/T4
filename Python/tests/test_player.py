e432ac57016dbbe1b482f711cb68bf73a360e475
Stas Medvedev <stan.medvedev@gmail.com>
2023-05-01 23:54:31 -0400

    Complete simple test for human player input

--- Python/tests/test_player.py
+++ Python/tests/test_player.py
@@ -1,5 +1,10 @@
 from Python import player
+from board import Board
 from pytest import MonkeyPatch
 
 def test_human_player(monkeypatch: MonkeyPatch) -> None:
     test_player = player.Player(name='Test', marker='X')
+    board = Board()
+    monkeypatch.setattr("builtins.input", "5")
+    position = test_player.take_turn(board)
+    assert position == 5
