bf17c5894a994c1af810efc03af230ebf1b4eb7f
Stas Medvedev <stan.medvedev@gmail.com>
2023-05-04 23:58:20 -0400

    Fix test_human_player test

--- Python/tests/test_player.py
+++ Python/tests/test_player.py
@@ -5,6 +5,6 @@ from pytest import MonkeyPatch
 def test_human_player(monkeypatch: MonkeyPatch) -> None:
     test_player = player.Player(name='Test', marker='X')
     board = Board()
-    monkeypatch.setattr("builtins.input", "5")
+    monkeypatch.setattr("builtins.input", lambda _: "5")
     position = test_player.take_turn(board)
     assert position == 5
