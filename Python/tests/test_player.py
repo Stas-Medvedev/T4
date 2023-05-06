67aa60a28537c832d48d8d68b941ca00c65c244a
Stas Medvedev <stan.medvedev@gmail.com>
2023-05-05 22:50:48 -0400

    Add simple test for Easy_CPU and a to-do comment

--- Python/tests/test_player.py
+++ Python/tests/test_player.py
@@ -2,9 +2,17 @@ import player
 from board import Board
 from pytest import MonkeyPatch
 
+# Look into parameterizing tests for CPU players with different boards
+
 def test_human_player(monkeypatch: MonkeyPatch) -> None:
     test_player = player.Player(name='Test', marker='X')
     board = Board()
     monkeypatch.setattr("builtins.input", lambda _: "5")
     position = test_player.take_turn(board)
     assert position == 5
+
+def test_Easy_cpu() -> None:
+    test_player = player.Easy_CPU_Player(name='CPU', marker='X')
+    board = Board()
+    position = test_player.take_turn(board)
+    assert position in board.available_positions
