3f68ce0eca2d87d01f1627c05618874ca72d47af
Stas Medvedev <stan.medvedev@gmail.com>
2023-05-06 23:57:28 -0400

    Update to-do item

--- Python/tests/test_player.py
+++ Python/tests/test_player.py
@@ -3,6 +3,8 @@ from board import Board
 from pytest import MonkeyPatch
 
 # Look into parameterizing tests for CPU players with different boards
+# Create multiple boards for different games scenarios
+# and test each CPU class on those boards (mostly, this is for Hard CPU)
 
 def test_human_player(monkeypatch: MonkeyPatch) -> None:
     test_player = player.Player(name='Test', marker='X')
