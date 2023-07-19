39c2f1414b7a72ea6c952ae97e8c15712939284d
Stas Medvedev <stan.medvedev@gmail.com>
2023-07-18 21:48:03 -0400

    Add test_from_string_blank

--- Python/tests/test_board.py
+++ Python/tests/test_board.py
@@ -1,17 +1,23 @@
 from board import Board
+import pytest
 
 def test_update_board() -> None:
     board = Board()
     board.update(1, 'X')
+    
     assert board.available_positions == list(range(2,10))
-    assert board.markers == ['X'] + [' ']*8
+    assert board.markers == ['X'] + [' '] * 8
 
 # TODO: Write several test for various possible from_string inputs
 # Can use some from test cases for player
 
-def test_from_string() -> None:
+def test_from_string_blank() -> None:
     board_string = (" | | "
                     "-+-+-"
                     " | | "
                     "-+-+-"
                     " | | ")
+    board = Board.from_string(board_string)
+    
+    assert board.available_positions == list(range(1,10))
+    assert board.markers == [' '] * 9
