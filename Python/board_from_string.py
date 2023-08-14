a02ca7c0bae502b9af8f5390da77b3fa0b50801d
Stas Medvedev <stan.medvedev@gmail.com>
2023-08-13 23:56:26 -0400

    Add board string

--- Python/board_from_string.py
+++ Python/board_from_string.py
@@ -1,4 +1,11 @@
 # File to check performance of the from_string method of the Board class
 from board import Board
 
-# TODO: Create board object from board string for the test that is not failing as it's supposed to
+board_string = \
+'''
+ | | 
+-+-+-
+ | | 
+-+-+-
+XXX| 
+'''
