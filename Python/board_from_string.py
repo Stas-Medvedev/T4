9ed3c155967542f884ad848932cee03ddd007dad
Stas Medvedev <stan.medvedev@gmail.com>
2023-08-16 22:33:49 -0400

    Add results of running the script

--- Python/board_from_string.py
+++ Python/board_from_string.py
@@ -10,7 +10,7 @@ board_string = \
 XXX| 
 '''
 
-board = Board.from_string(board_string)
+board = Board.from_string(board_string)     # raises "ValueError: Invalid marker passed: XXX"
 
 print(f"Markers: {board.markers}")
 print(f"Available positions: {board.available_positions}")
