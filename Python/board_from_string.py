4a3a5668619b55748d0c3ad32ec570ba4c137906
Stas Medvedev <stan.medvedev@gmail.com>
2023-08-22 23:59:35 -0400

    Add to-do item

--- Python/board_from_string.py
+++ Python/board_from_string.py
@@ -14,3 +14,5 @@ board = Board.from_string(board_string)     # raises "ValueError: Invalid marker
 
 print(f"Markers: {board.markers}")
 print(f"Available positions: {board.available_positions}")
+
+# TODO: Add test for missing space marker
