a97209a32f76450a499a3247e456c7c9ec4718f3
Stas Medvedev <stan.medvedev@gmail.com>
2024-02-24 22:11:04 -0500

    Complete test_get_player_selection

--- Python/tests/test_ui.py
+++ Python/tests/test_ui.py
@@ -1,5 +1,8 @@
 from ui import UI
+from pytest import MonkeyPatch
 
-def test_get_plyer_selection():
-    # TODO: Use monkeypatch to simulate input. Look at test_player for reference.
-    pass
+def test_get_plyer_selection(monkeypatch: MonkeyPatch) -> None:
+    inputs = iter(['X', '3'])
+    monkeypatch.setattr("builtins.input", lambda _: next(inputs))
+    choice = UI.get_player_selection()
+    assert choice == ("cpu", "human")
