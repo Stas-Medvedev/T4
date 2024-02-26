82021fc08f3ac01356853115809489ba1eee299b
Stas Medvedev <stan.medvedev@gmail.com>
2024-02-25 21:56:48 -0500

    Add a to-do item

--- Python/tests/test_ui.py
+++ Python/tests/test_ui.py
@@ -2,6 +2,7 @@ from ui import UI
 from pytest import MonkeyPatch
 
 def test_get_plyer_selection(monkeypatch: MonkeyPatch) -> None:
+    # TODO: Add checks for other return values
     inputs = iter(['X', '3'])
     monkeypatch.setattr("builtins.input", lambda _: next(inputs))
     choice = UI.get_player_selection()
