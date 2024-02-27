296e51462aedde1b2bbe1a29ecbcd5275fc2e6bd
Stas Medvedev <stan.medvedev@gmail.com>
2024-02-26 21:36:19 -0500

    Rename test_get_player_selection and add a to-do item

--- Python/tests/test_ui.py
+++ Python/tests/test_ui.py
@@ -1,8 +1,9 @@
 from ui import UI
 from pytest import MonkeyPatch
 
-def test_get_plyer_selection(monkeypatch: MonkeyPatch) -> None:
-    # TODO: Add checks for other return values
+# TODO: Write tests for other player selection choices
+
+def test_get_plyer_selection_3(monkeypatch: MonkeyPatch) -> None:
     inputs = iter(['X', '3'])
     monkeypatch.setattr("builtins.input", lambda _: next(inputs))
     choice = UI.get_player_selection()
