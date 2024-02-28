2fafea1d3e7d0a67fb69856fdf8f711d1991b576
Stas Medvedev <stan.medvedev@gmail.com>
2024-02-27 23:11:37 -0500

    Add test_get_player_selection_2

--- Python/tests/test_ui.py
+++ Python/tests/test_ui.py
@@ -3,7 +3,13 @@ from pytest import MonkeyPatch
 
 # TODO: Write tests for other player selection choices
 
-def test_get_plyer_selection_3(monkeypatch: MonkeyPatch) -> None:
+def test_get_player_selection_2(monkeypatch: MonkeyPatch) -> None:
+    inputs = iter(['a', '8', '2'])
+    monkeypatch.setattr("builtins.input", lambda _: next(inputs))
+    choice = UI.get_player_selection()
+    assert choice == ("human", "cpu")
+
+def test_get_player_selection_3(monkeypatch: MonkeyPatch) -> None:
     inputs = iter(['X', '3'])
     monkeypatch.setattr("builtins.input", lambda _: next(inputs))
     choice = UI.get_player_selection()
