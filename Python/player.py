f85c57e1572e0276a44616bce71aa485f100d4d1
Stas Medvedev <stan.medvedev@gmail.com>
2024-03-23 21:35:45 -0400

    Add difficulty attribute to Hard_CPU_Player

--- Python/player.py
+++ Python/player.py
@@ -32,6 +32,7 @@ class Hard_CPU_Player(Player):
     '''
     Hard CPU player follows an algorithm to make sure to not lose a game
     '''
+    difficulty = 'hard'
     WINNING_POSITIONS = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]]
 
     def can_win(self, board: Board) -> int:
