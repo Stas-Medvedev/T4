1eb264549ec9cf2683b00e0840f2bf2b2e114acf
Stas Medvedev <stan.medvedev@gmail.com>
2024-03-27 20:48:31 -0400

    Add a to-do item

--- Python/tests/test_game_manager.py
+++ Python/tests/test_game_manager.py
@@ -2,4 +2,6 @@ import pytest
 from game_manager import GameManager
 
 def test_get_cpu_player_object_easy() -> None:
+    # TODO: Figure out a way to mock inputs for both marker and ui.select_CPU_difficulty.
+    # Although marker can probably just be provided in the function call, so no need to mock it.
     pass
